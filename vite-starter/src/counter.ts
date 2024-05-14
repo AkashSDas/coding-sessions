89e146f2a92c5dc4dff5a9e5db88e96a2f4a4b3e
AkashSDas <aakashdas368@gmail.com>
2024-05-14 21:36:32 +0530

    [vite-starter] init

---
+++ vite-starter/src/counter.ts
@@ -0,0 +1,25 @@
+export function initializeCounter(doc = globalThis.document): void {
+    const countElement = doc.getElementById("count");
+    const incrementButton = doc.getElementById("increment");
+    const decrementButton = doc.getElementById("decrement");
+
+    let count = 0;
+    incrementButton?.addEventListener("click", increment);
+    decrementButton?.addEventListener("click", decrement);
+
+    function render(): void {
+        if (countElement) {
+            countElement.textContent = count.toString();
+        }
+    }
+
+    function increment(): void {
+        count++;
+        render();
+    }
+
+    function decrement(): void {
+        count--;
+        render();
+    }
+}
---
+++ vite-starter/src/index.ts
@@ -0,0 +1,26 @@
+const rootElement = document.getElementById("app");
+
+// uncomment the dynamic import or the static import and run pnpm run build
+// to see how the production build will end up looking like.
+
+// Will bundle index.ts and counter.ts into a single file since its small and is immediately used
+import { initializeCounter } from "./counter";
+initializeCounter(rootElement?.ownerDocument);
+
+// Output for above import statement:
+// ✓ 4 modules transformed.
+// dist/index.html                0.82 kB │ gzip: 0.39 kB
+// dist/assets/index-clmwRAp8.js  1.09 kB │ gzip: 0.56 kB
+// ✓ built in 67ms
+
+// Dynamically import counter.ts
+import("./counter").then((module) => {
+    module.initializeCounter(rootElement?.ownerDocument);
+});
+
+// Output for above import statement:
+// ✓ 5 modules transformed.
+// dist/index.html                  0.82 kB │ gzip: 0.39 kB
+// dist/assets/counter-eMei2_UA.js  0.34 kB │ gzip: 0.22 kB
+// dist/assets/index-BYphEXjm.js    1.77 kB │ gzip: 0.91 kB
+// ✓ built in 59ms
