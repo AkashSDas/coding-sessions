ba53d2d15b191d2a029a0e4e5549e03b2dd03a8e
AkashSDas <aakashdas368@gmail.com>
2024-05-15 09:58:43 +0530

    [vite-starter] add vite-imagetools and optimize images

--- vite-starter/src/index.ts
+++ vite-starter/src/index.ts
@@ -4,8 +4,8 @@ const rootElement = document.getElementById("app");
 // to see how the production build will end up looking like.
 
 // Will bundle index.ts and counter.ts into a single file since its small and is immediately used
-import { initializeCounter } from "./counter";
-initializeCounter(rootElement?.ownerDocument);
+// import { initializeCounter } from "./counter";
+// initializeCounter(rootElement?.ownerDocument);
 
 // Output for above import statement:
 // ✓ 4 modules transformed.
@@ -24,3 +24,21 @@ initializeCounter(rootElement?.ownerDocument);
 // dist/assets/counter-eMei2_UA.js  0.34 kB │ gzip: 0.22 kB
 // dist/assets/index-BYphEXjm.js    1.77 kB │ gzip: 0.91 kB
 // ✓ built in 59ms
+
+// import cityImg from "./city.avif";
+// @ts-ignore
+// import cityImg from "./city.avif?h=400&format=webp&quality=80&fit=fill&as=metadata";
+import cityImg from "./city.avif?h=400;800";
+console.log({ cityImg }); // its the img url or {cityImg: Array(2)} in case of h=400;800
+const img = rootElement?.ownerDocument.createElement("img");
+if (img) {
+    img.src = cityImg[0];
+    rootElement?.appendChild(img);
+}
+
+// import { initializeCounter } from "./counter";
+// console.log(initializeCounter); // function
+
+// @ts-ignore
+import styles from "./alert.css?url";
+console.log(styles);
